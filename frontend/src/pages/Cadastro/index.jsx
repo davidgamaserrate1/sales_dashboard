e30af86ba03bedd032719f0e5d9a1769118fdc49
davidgamaserrate1 <david.gama@ufms.br>
2023-09-09 18:27:57 -0300

    feat: salva dados do formulário

--- frontend/src/pages/Cadastro/index.jsx
+++ frontend/src/pages/Cadastro/index.jsx
@@ -1,32 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import style from './login-styles.module.css'
 import ModalAuth from "../../components/modalAuth";
-import { Button, FormControl, Heading, Input } from "@chakra-ui/react";
+import { Button, FormControl, FormErrorMessage, Heading, Input } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 
 const Login = () =>{
+    const [email, setEmail] =useState('')
+    const [password, setPassword]=useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+
+    
+
     const navigate = useNavigate()
+
+    const isError = password !== confirmPassword 
+    
+    const handleRegister = async()=>{
+        const credentialsRegister ={
+            email,
+            password
+        }
+        await fetch('', {
+            body: JSON.stringify(credentialsRegister)
+        })
+        .then((res)=>res.json())
+        .then((response)=> console.log(response))
+    }
+
+
     return( 
         <ModalAuth>
         <div className={style.login_container}>
             <Heading   as='h3' size='lg' color={'#555555'}>Cadastro </Heading>
-            <FormControl mt={8} >
                 <Input type='email' mt={8} 
                     placeholder="Endereço de e-mail."
+                    value={email}
+                    onChange={(e)=>setEmail(e.target.value)}
                 />
+            <FormControl isInvalid={isError}  >
                 <Input type='password'mt={8} 
                     placeholder="Digite uma senha."
+                    value={password}
+                    onChange={(e)=>setPassword(e.target.value)}
                 />
                 <Input type='password' mt={8}
-                    placeholder="Confirmar senha."/>
+                    placeholder="Confirmar senha."
+                    value={confirmPassword}
+                    onChange={(e)=>setConfirmPassword(e.target.value)}
+                    
+                    />
+                    <FormErrorMessage>
+                   As senhas nao correspondem
+                </FormErrorMessage>
                 <Button
                     mt={8}
                     colorScheme='teal'
                     width={'100%'}
                     type='submit'
+                    onClick={handleRegister}
                 >
                 Cadastrar
                 </Button>
+                
             </FormControl>
 
             <div className={style.login_redirect}>
